f6dcc1bc60c9b4d307f7fce1e9d9de74d2dc34ff
Adhya739 <adhya.24bsa10319@vitbhopal.ac.in>
2025-11-24 12:15:54 +0530

    Create ClinicSystemTest.java

---
+++ ClinicSystemTest.java
@@ -0,0 +1,6 @@
+public class ClinicSystemTest {
+    public static void main(String[] args) {
+        // Example tests (expand as needed)
+        System.out.println("ClinicSystem tests placeholder.");
+    }
+}
